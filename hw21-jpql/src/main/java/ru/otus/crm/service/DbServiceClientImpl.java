2122bc80ebb7e6781f1c51ad0d33aaab0d834ef9
Bliprog <blinprog@gmail.com>
2022-01-29 02:17:26 +0300

    delete unusable imports

--- hw21-jpql/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
+++ hw21-jpql/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
@@ -3,12 +3,9 @@ package ru.otus.crm.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.otus.core.repository.DataTemplate;
-import ru.otus.crm.model.Client;
 import ru.otus.core.sessionmanager.TransactionManager;
+import ru.otus.crm.model.Client;
 
-import javax.persistence.EntityGraph;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,7 +39,7 @@ public class DbServiceClientImpl implements DBServiceClient {
     @Override
     public Optional<Client> getClient(long id) {
         return transactionManager.doInReadOnlyTransaction(session -> {
-            var query = session.createQuery("select c from Client c join fetch c.phones join fetch c.address where c.id="+id,Client.class);
+            var query = session.createQuery("select c from Client c join fetch c.phones join fetch c.address where c.id=" + id, Client.class);
             Client client = query.getSingleResult();
             Optional<Client> result = Optional.of(client);
             log.info("client: {}", client);
@@ -53,10 +50,10 @@ public class DbServiceClientImpl implements DBServiceClient {
     @Override
     public List<Client> findAll() {
         return transactionManager.doInReadOnlyTransaction(session -> {
-            var query = session.createQuery("select distinct c from Client c join fetch c.phones  join fetch c.address",Client.class);
+            var query = session.createQuery("select distinct c from Client c join fetch c.phones  join fetch c.address", Client.class);
             List<Client> clients = query.getResultList();
             log.info("clientList:{}", clients);
             return clients;
-       });
+        });
     }
 }
