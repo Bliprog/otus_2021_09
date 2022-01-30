535bf0f9f6c7a20d15b84a9fb3b2bb17d176cb63
Bliprog <blinprog@gmail.com>
2022-01-30 22:59:32 +0300

    change language in TestStatisticConsoleWriter

--- hw03-reflections/src/main/java/ru/bliprog/statistics/TestStatisticConsoleWriter.java
+++ hw03-reflections/src/main/java/ru/bliprog/statistics/TestStatisticConsoleWriter.java
@@ -6,12 +6,12 @@ public class TestStatisticConsoleWriter implements TestStatisticWriter {
         var failedTestsWithException = testStatistic.getFailedTestsWithException();
         var passedTests = testStatistic.getPassedTests();
         StringBuilder outputBuilder = new StringBuilder();
-        outputBuilder.append(String.format("Всего тестов: %s\n", failedTestsWithException.size() + passedTests.size()));
-        outputBuilder.append(String.format("Пройдено тестов: %s\n", passedTests.size()));
-        outputBuilder.append(String.format("Не пройдено тестов: %s\n", failedTestsWithException.size()));
-        outputBuilder.append("Далее приведена статистика по не пройденным тестам\n");
+        outputBuilder.append(String.format("Total tests: %s\n", failedTestsWithException.size() + passedTests.size()));
+        outputBuilder.append(String.format("Passed tests: %s\n", passedTests.size()));
+        outputBuilder.append(String.format("Failed Tests: %s\n", failedTestsWithException.size()));
+        outputBuilder.append("Failed test statistic:\n");
         for (var entry : failedTestsWithException.entrySet()) {
-            outputBuilder.append(String.format("Тест: %s  Исключение: %s\n", entry.getKey().getName(), entry.getValue()));
+            outputBuilder.append(String.format("Test name: %s  Exception: %s\n", entry.getKey().getName(), entry.getValue()));
         }
         System.out.println(outputBuilder);
     }
