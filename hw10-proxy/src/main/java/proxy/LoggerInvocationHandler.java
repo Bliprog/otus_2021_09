ae8b449c684ea45a40b804e05f6d158ce964df36
Bliprog <blinprog@gmail.com>
2022-03-13 16:25:09 +0300

    fix

--- hw10-proxy/src/main/java/proxy/LoggerInvocationHandler.java
+++ hw10-proxy/src/main/java/proxy/LoggerInvocationHandler.java
@@ -4,19 +4,28 @@ import annotation.Log;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LoggerInvocationHandler implements InvocationHandler {
     private final Object defaultObject;
+    private final List<Method> methodsForLogging;
 
     LoggerInvocationHandler(Object o) {
         this.defaultObject = o;
+        this.methodsForLogging = Arrays.stream(defaultObject.getClass().getDeclaredMethods())
+                .filter(method -> method.getAnnotation(Log.class) != null)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
-        var logAnnotation = defaultObject.getClass().getMethod(method.getName(), method.getParameterTypes()).getAnnotation(Log.class);
-        if (logAnnotation != null) {
+        if (methodsForLogging.stream()
+                .filter(m -> m.getName().equals(method.getName()))
+                .anyMatch(m -> Arrays.equals(m.getParameterTypes(), method.getParameterTypes()))
+        ) {
             StringBuilder sb = new StringBuilder();
             sb.append(String.format("Class:%s, executed method:%s, params:", defaultObject.getClass().getName(), method.getName()));
             for (var arg : args) {
